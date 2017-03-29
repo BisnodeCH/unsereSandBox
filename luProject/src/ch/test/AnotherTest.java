255e876cbfa3a1c96b939b11ff8697889da76e35
Luca Comparato <luca.comparato@bisnode.com>
2017-03-29 15:37:21 +0200

    Initial revision.

--- luProject/src/ch/test/AnotherTest.java
+++ luProject/src/ch/test/AnotherTest.java
@@ -1,5 +1,14 @@
 package ch.test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class AnotherTest {
+	
+	@Test
+	public void doTest() {
+		assertTrue(true);
+	}
 
 }
